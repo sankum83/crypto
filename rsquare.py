b219f2fd42bcf6dc3029a692f6f50fe41f32397b
= <kumarsanjay83@gamil.com>
2018-08-24 05:35:44 +0000

    updated rsquare.py

--- rsquare.py
+++ rsquare.py
@@ -1,3 +1,18 @@
 #R-Square
 
 print "Geting R-Square"
+#X coordinate 
+xcoord = [1,5,9,2,7,5]
+#Y coordinate 
+ycoord = [2,4,7,4,2,9]
+
+xlen = len(xcoord)
+ylen = len(ycoord)
+
+if xlen == ylen:
+    print "Coordinates are same lenght"
+else:
+    print "Coordinates are diffrent lenght"
+
+#get the mean of xcoord and ycoord
+
