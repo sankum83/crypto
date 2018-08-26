eb9fffb7254fb1b8e4a6b33896e60ac815bcf539
= <kumarsanjay83@gamil.com>
2018-08-26 10:55:15 +0000

    Code Changes

--- rsquare.py
+++ rsquare.py
@@ -4,7 +4,7 @@ print "Geting R-Square"
 #X coordinate 
 xcoord = [1,5,9,2,7,5]
 #Y coordinate 
-ycoord = [2,4,7,4,2,9]
+ycoord = [2,4,7,4,8,9]
 
 xlen = len(xcoord)
 ylen = len(ycoord)
@@ -15,4 +15,29 @@ else:
     print "Coordinates are diffrent lenght"
 
 #get the mean of xcoord and ycoord
+xmean = sum(xcoord)
+ymean = sum(ycoord)
+
+xmean = xmean/xlen
+ymean = ymean/ylen
+
+print ("X-Mean: %.5f , and Y-Mean: %.5f"%(xmean, ymean))
+
+#print(xmean)
+print " x bar "
+xbar = []
+ybar = []
+
+
+for x in xcoord:
+    xbar.append(xmean - x)
+
+for y in ycoord:
+    ybar.append(ymean - y)
+
+
+print xbar
+
+print ybar
+
 
